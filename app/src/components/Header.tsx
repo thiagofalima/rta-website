9031702456bed5a649b6e524d107b02896aa5cde
thiagofalima <thiagofalima@hotmail.com>
2026-03-02 09:48:48 -0300

    feat: add Header and ChakraUI setup

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,1 +1,17 @@
 
+import styled from "styled-components"
+
+const HeaderWrapper = styled.div`
+    width: 100vw;
+    color: white;
+    padding: 16px;
+    background-color: blue;
+`
+
+export const Header = () => {
+    return (
+        <HeaderWrapper>
+            Hello
+        </HeaderWrapper>
+    )
+}
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "@/components/ui/provider";
+import { Provider } from "./components/ui/provider.tsx";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
