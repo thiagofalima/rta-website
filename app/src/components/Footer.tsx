43c60f3a36851a6be91bf29f5c191ee905f79d86
thiagofalima <thiagofalima@hotmail.com>
2026-03-02 10:05:01 -0300

    feat(footer): create base Footer - Also add Footer to Layout

---
+++ app/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+
+export const Footer = () => {
+  return (
+    <>
+      <p>Footer</p>
+    </>
+  );
+};
--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -1,10 +1,14 @@
 import { Header } from "./Header";
+import { Footer } from "./Footer";
+
+
 
 export const Layout = ({ children }: any) => {
     return (
         <>
         <Header />
         { children }
+        <Footer />
         </>
     )
 }
