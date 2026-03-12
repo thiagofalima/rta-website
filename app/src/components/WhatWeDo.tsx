97cfeea962865e495d64b8e4b0604bb383401eec
thiagofalima <thiagofalima@hotmail.com>
2026-03-12 10:42:23 -0300

    feat: add small instruction - aldo add an animated left arrow on the small

--- app/src/components/WhatWeDo.tsx
+++ app/src/components/WhatWeDo.tsx
@@ -1,5 +1,11 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { Products } from "./Products";
+
+const moveArrow = keyframes`
+  0% { transform: translateX(0); }
+  50% { transform: translateX(5px); }
+  100% { transform: translateX(0); }
+`;
 
 const WhatWeDoWrapper = styled.section`
   width: 100vw;
@@ -14,11 +20,27 @@ const WhatWeDoWrapper = styled.section`
     padding: 16px;
   }
 
+  h2.title,
+  small {
+    color: #ffffff;
+  }
+
+  small {
+    margin-bottom: 10px;
+    display: block;
+    font-size: 14px;
+    &::after {
+      content: " \\2192"; /* Seta para a direita (→) */
+      display: inline-block;
+      margin-left: 5px;
+      animation: ${moveArrow} 1.5s ease-in-out infinite;
+    }
+  }
+
   h2.title {
     font-size: 1.5em;
     font-weight: 700;
-    margin: 16px 0;
-    color: #FFFFFF;
+    margin: 10px 0;
   }
 `;
 
@@ -27,6 +49,7 @@ export const WhatWeDo = () => {
     <WhatWeDoWrapper id="WhatWeDo">
       <div className="container">
         <h2 className="title">O que fazemos?</h2>
+        <small>Passe para o lado</small>
         <Products />
       </div>
     </WhatWeDoWrapper>
